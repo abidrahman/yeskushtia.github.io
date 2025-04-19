886b01b95532b6887a166a7c6d0e48d57acb9672
abidrahman <arahman1@ualberta.ca>
2025-04-19 02:11:48 -0400

    cleanup

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,5 +1,3 @@
----
----
 $(function() {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
@@ -40,7 +38,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Thank you for your message! We'll get back to you soon.</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -52,7 +50,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", there seems to be an issue with our contact form. Please try reaching out through our social media channels instead."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
